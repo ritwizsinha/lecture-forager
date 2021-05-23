b6676f3279cc096980dacef0039024a2e63c3233
Ritwiz Sinha <ritwizsinha0@gmail.com>
2021-05-23 22:27:35 +0530

    Create indexes for full text search and otherwise in database intialization

--- server/init.sql
+++ server/init.sql
@@ -1,10 +1,34 @@
+DROP TABLE videos;
 CREATE TABLE videos (
     id SERIAL PRIMARY KEY,
-    filename varchar,
-    storage_id varchar,
-    audio_id varchar,
-    title varchar,
-    description varchar,
+    filename varchar NOT NULL ,
+    storage_id varchar UNIQUE ,
+    audio_id varchar UNIQUE ,
+    title varchar NOT NULL ,
+    description varchar NOT NULL ,
     posted_at TIMESTAMP DEFAULT current_timestamp,
-    keywords text
+    keywords text,
+    document_with_weights tsvector
 );
+
+CREATE INDEX storage_id_idx
+ON videos(storage_id);
+
+CREATE INDEX audio_id_idx
+ON videos(audio_id);
+
+CREATE INDEX document_idx_weights
+ON videos(document_with_weights);
+
+CREATE FUNCTION videos_tsvector_trigger() RETURNS TRIGGER AS $$
+begin
+    new.document_with_weights :=
+    setweight(to_tsvector('english', coalesce(new.title, '')), 'A')
+    || setweight(to_tsvector('english', coalesce(new.description, '')), 'B')
+    || setweight(to_tsvector('english', coalesce(new.keywords, '')), 'C');
+    return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+    ON videos FOR EACH ROW EXECUTE PROCEDURE videos_tsvector_trigger()
